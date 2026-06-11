b2b413fc6de95765fe27230fe8caa90d73c5568b
Aditya-Rastogi-new <anmol900514arjun@gmail.com>
2026-06-11 22:46:33 +0530

    Create reverse-words-in-a-string-iii.java

---
+++ Week_2/Day_4/reverse-words-in-a-string-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void reverse(char [] arr,int i, int j){
+        while(i<j){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int start = 0;
+        
+        for(int end = 0; end<=n; end++){
+            if(end==n || arr[end] == ' '){
+                reverse(arr,start,end-1);
+                start = end + 1;
+            }
+        }
+        return new String(arr);
+    }
+}
