e3260ecb62c5a43284fce6056936512533a2d222
Aditya-Rastogi-new <anmol900514arjun@gmail.com>
2026-06-13 15:11:46 +0530

    Create Veena.java

---
+++ Week_2/OOPS/OOPS_Questions_2/music/string/Veena.java
@@ -0,0 +1,10 @@
+package OOPS_Questions_2.music.string;
+
+import OOPS_Questions_2.music.Playable;
+
+public class Veena implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
