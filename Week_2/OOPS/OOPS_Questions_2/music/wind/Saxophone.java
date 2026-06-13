d9afee6a89c8be4834aaf1242187899ad033cecb
Aditya-Rastogi-new <anmol900514arjun@gmail.com>
2026-06-13 15:17:40 +0530

    Update Saxophone.java

--- Week_2/OOPS/OOPS_Questions_2/music/wind/Saxophone.java
+++ Week_2/OOPS/OOPS_Questions_2/music/wind/Saxophone.java
@@ -1,1 +1,11 @@
+package OOPS_Questions_2.music.wind;
+
+import OOPS_Questions_2.music.Playable;
+
+public class Saxophone implements Playable {
+    @Override
+    public void play(){
+        System.out.println("Playing Saxophone");
+    }
+}
 
